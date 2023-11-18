13ac8356c59fa97af7fa61be75b2a86fb28e1047
RenHartt <141726227+RenHartt@users.noreply.github.com>
2023-11-18 18:48:36 +0100

    Add files via upload

---
+++ Get_next_line/get_next_line.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgoron <bgoron@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/13 19:25:43 by bgoron            #+#    #+#             */
+/*   Updated: 2023/11/18 18:48:03 by bgoron           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*newstatic(char *str)
+{
+	size_t	i;
+	char	*tmp;
+
+	i = 0;
+	if (!str)
+		return (ft_calloc(1, 1));
+	while (str[i] && str[i] != '\n') 
+		i++;
+	tmp = ft_substr(str, i + 1, ft_strlen(str) - i);
+	free(str);
+	return (tmp);
+}
+
+char	*fill(char *str1, char *str2)
+{
+	size_t	i;
+
+	i = 0;
+	while (str1[i] && str1[i] != '\n')
+		i++;
+	str2 = ft_calloc(i + 2, sizeof(char));
+	if (!str1)
+		return (NULL);
+	i = 0;
+	while (str1[i])
+	{
+		str2[i] = str1[i];
+		if (str1[i] == '\n')
+		{
+			i++;
+			break;
+		}
+		i++;
+	}
+	str2[i] = '\0';
+	return (str2);
+}
+
+char	*rfile(int fd, char *line)
+{
+	int		r;
+	char	*curent;
+
+	r = 1;
+	curent = ft_calloc(sizeof(char), (BUFFER_SIZE + 1));
+	if (!curent)
+		return (NULL);
+	while (!ft_strchr(curent, '\n') && r != 0)
+	{
+		r = read(fd, curent, BUFFER_SIZE);
+		if (r <= 0)
+		{
+			free(curent);
+			return (line);
+		}
+		curent[r] = '\0';
+		line = ft_strjoin(line, curent);
+	}
+	free(curent);
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*line;
+	char 		*tmp;
+
+	tmp = NULL;
+	line = rfile(fd, line);
+	if (line == NULL)
+		return (NULL);
+	tmp = fill(line, tmp);
+	line = newstatic(line);
+	if (!ft_strchr(tmp, '\n'))
+	{
+		free(line);
+		if (!*tmp)
+		{
+			free(tmp);
+			tmp = NULL;
+		}
+		line = NULL;
+	}
+	return (tmp);
+}
---
+++ Get_next_line/get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgoron <bgoron@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/13 19:25:32 by bgoron            #+#    #+#             */
+/*   Updated: 2023/11/18 18:46:39 by bgoron           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 0
+#endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *s);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_substr(char *s, size_t start, size_t len);
+char	*rfile(int fd, char *line);
+char	*fill(char *str1, char *str2);
+char	*newstatic(char *str);
+
+#endif
